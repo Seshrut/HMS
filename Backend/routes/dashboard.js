c18ad7bb35c2b8b465eec812ad4b49b714c3d70d
Seshrut <seshrutjain@gmail.com>
2026-03-28 22:41:00 +0530

    temporary addition for testing

--- Backend/routes/dashboard.js
+++ Backend/routes/dashboard.js
@@ -9,6 +9,7 @@ router.get("/stats", authenticateToken, allowRoles("doctor"), (req, res) => {
     Retrieves aggregated data such as
     total appointments, pending appointments, approved appointments, and completed consultations.
   */
+ res.json({Todayappointments:24,patients:120,prescriptions:18,pendingReports:6});
 });
 
 export default router;
